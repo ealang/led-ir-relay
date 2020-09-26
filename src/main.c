4005576e10a22687680e045b7ea0e09dd9f661db
elang <ealang@gmail.com>
2020-09-26 18:22:23 -0400

    IR mirroring prototype

--- src/main.c
+++ src/main.c
@@ -1,15 +1,32 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 
+ISR(PCINT0_vect)
+{
+	// TODO: check which pin got set (?)
+	PORTC = (PORTC & ~2) | (((~PINB) & 1) << 1);
+}
 
+/*
+
+PC0 blink
+PC1 mirrored output
+PB0/PCINT0 input
+*/
 int main(void)
 {
-	DDRC = 0xFF;
+	DDRC = 0x3;
+	PORTC = 1;
+	
+	DDRB = 0;
+	PCMSK0 |= 1;
+	PCICR |= 1;
+
+	sei();
 	while(1)
 	{
-		PORTC = 0xFF;
-		_delay_ms(1000);
-		PORTC= 0x00;
+		PORTC ^= 1;
 		_delay_ms(1000);
 	}
 }
