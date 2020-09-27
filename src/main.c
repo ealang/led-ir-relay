135d318933b8d16e7bfadc05ece50c6841a99cfc
Eric Lang <ealang@gmail.com>
2020-09-27 16:51:41 -0400

    Threading prototype

--- src/main.c
+++ src/main.c
@@ -1,32 +1,93 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <assert.h>
 #include <avr/interrupt.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
 
-ISR(PCINT0_vect)
+#define MAX_THREADS 5
+
+// Bytes needed to preserve current stack frame
+#define FRAME_BACKUP_SIZE 21
+#define THREAD_STACK_SIZE 0x100
+
+typedef struct
 {
-	// TODO: check which pin got set (?)
-	PORTC = (PORTC & ~2) | (((~PINB) & 1) << 1);
+	uint8_t *sp;
+	uint8_t stack[THREAD_STACK_SIZE];
+} Thread;
+
+typedef struct
+{
+	Thread *threads[MAX_THREADS];
+	uint8_t n_threads;
+} Scheduler;
+
+extern void scheduler_resume_thread(Thread *to);
+extern void scheduler_switch_threads(Thread *from, Thread *to);
+
+Scheduler *scheduler_inst;
+
+void scheduler_init(Scheduler *scheduler)
+{
+	scheduler->n_threads = 0;
 }
 
-/*
+uint8_t scheduler_register_thread(Scheduler *scheduler, Thread *coroutine, void (*start)())
+{
+	// Init coroutine stack
+	memset((void *)coroutine->stack, 0, THREAD_STACK_SIZE);
+	coroutine->sp = coroutine->stack + THREAD_STACK_SIZE - FRAME_BACKUP_SIZE - 1;
+	// Setup return address
+	coroutine->stack[THREAD_STACK_SIZE - 2] = (uint16_t)start >> 8;
+	coroutine->stack[THREAD_STACK_SIZE - 1] = (uint16_t)start & 0xFF;
 
-PC0 blink
-PC1 mirrored output
-PB0/PCINT0 input
-*/
+	// Register with scheduler
+	assert(scheduler->n_threads < MAX_THREADS);
+	int id = scheduler->n_threads++;
+	scheduler->threads[id] = coroutine;
+	
+	return id;
+}
+
+void test1()
+{
+	uint16_t x = 0;
+	while (1)
+	{
+		x += 5;
+		printf("test1 %x", x);
+		scheduler_switch_threads(scheduler_inst->threads[0], scheduler_inst->threads[1]);
+	}
+}
+
+void test2()
+{
+	uint16_t x = 0;	
+	while (1)
+	{
+		x += 1;
+		printf("test2 %x", x);
+		scheduler_switch_threads(scheduler_inst->threads[1], scheduler_inst->threads[0]);
+	}
+}
+
 int main(void)
 {
-	DDRC = 0x3;
-	PORTC = 1;
-	
-	DDRB = 0;
-	PCMSK0 |= 1;
-	PCICR |= 1;
-
 	sei();
-	while(1)
-	{
-		PORTC ^= 1;
-		_delay_ms(1000);
-	}
+
+	Scheduler scheduler;
+	scheduler_init(&scheduler);
+
+	Thread t1;
+	scheduler_register_thread(&scheduler, &t1, test1);
+
+	Thread t2;
+	scheduler_register_thread(&scheduler, &t2, test2);
+
+	scheduler_inst = &scheduler;
+
+	scheduler_resume_thread(&t1);
 }
---
+++ src/scheduler.s
@@ -0,0 +1,79 @@
+//.include "m328pdef.inc"
+
+// Switch from one thread to another.
+// @param thread_from pointer to current thread struct
+// @param thread_to pointer to other thread struct
+.global scheduler_switch_threads
+scheduler_switch_threads:
+
+	// Push registers
+	push r0
+	push r2
+	push r3
+	push r4
+	push r5
+	push r6
+	push r7
+	push r8
+	push r9
+	push r10
+	push r11
+	push r12
+	push r13
+	push r14
+	push r15
+	push r16
+	push r17
+	push r28
+	push r29
+
+	// Save stack pointer
+	mov xh, r25  // param1 h
+	mov xl, r24  // param1 l
+
+	in r18, 0x3d  // SPL
+	st X+, r18
+	in r18, 0x3e  // SPH
+	st X, r18
+
+	// shift param2 to param1
+	mov r25, r23
+	mov r24, r22
+
+
+// Switch to given thread
+// @param thread pointer to Thread struct
+.global scheduler_resume_thread
+scheduler_resume_thread:
+
+	// Restore stack pointer
+	mov xh, r25  // param1 h
+	mov xl, r24  // param1 l
+
+	ld r18, X+
+	out 0x3d, r18  // SPL
+	ld r18, X
+	out 0x3e, r18  // SPH
+
+	// Pop registers
+	pop r29
+	pop r28
+	pop r17
+	pop r16
+	pop r15
+	pop r14
+	pop r13
+	pop r12
+	pop r11
+	pop r10
+	pop r9
+	pop r8
+	pop r7
+	pop r6
+	pop r5
+	pop r4
+	pop r3
+	pop r2
+	pop r0
+
+	ret
